bb8fd6627fba658a781ae9445996879b140fd32a
eashishir <eftekharahmedshishir@gmail.com>
2022-11-09 02:23:03 -0800

    data load in mongodb complete

--- index.js
+++ index.js
@@ -15,11 +15,26 @@ const port = process.env.PORT || 5000 ;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4qgafns.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run () {
+    try{
+        const serviceCollection = client.db('assignment11').collection('services');
+
+        app.get('/services',async(req,res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await  cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(err => console.error(err));
 
 
  app.get('/', (req,res) => {
